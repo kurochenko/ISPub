fc0b9e6f3f9957574841a3611ff9ff77158d38dd
Andrej Kuročenko <kurochenko@gmail.com>
2011-07-10 15:37:18 +0200

    feature: implemented parser of CSV files with authors

--- src/main/java/net/kurochenko/ispub/upload/AuthorParserCSV.java
+++ src/main/java/net/kurochenko/ispub/upload/AuthorParserCSV.java
@@ -2,11 +2,11 @@ package net.kurochenko.ispub.upload;
 
 import au.com.bytecode.opencsv.CSVReader;
 import net.kurochenko.ispub.author.form.Author;
+import net.kurochenko.ispub.department.form.Department;
+import net.kurochenko.ispub.source.form.Source;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /**
  * User: kurochenko
@@ -27,15 +27,63 @@ public class AuthorParserCSV {
 
         String [] line;
         Author author = null;
+        String department = null;
+        List<String> sourceNames = null;
 
         while ((line = reader.readNext()) != null) {
+
+            /* Line doesn't contain required values */
+            if (line.length < AuthorCSVColumnNumbers.SOURCES.getColumnNumber()) {
+                continue;
+            }
+
             author = new Author();
-            author.setName(line[AuthorCSVColumnNumbers.NAME.getColumnNumber()]);
-            author.setSurname(line[AuthorCSVColumnNumbers.SURNAME.getColumnNumber()]);
+            author.setName(getValue(line, AuthorCSVColumnNumbers.NAME.getColumnNumber()));
+            author.setSurname(getValue(line, AuthorCSVColumnNumbers.SURNAME.getColumnNumber()));
+
+            department = getValue(line, AuthorCSVColumnNumbers.DEPARTMENT.getColumnNumber());
+            if (department != null) {
+                author.setDepartment(new Department(department));
+            }
+
+            sourceNames = getValues(line, AuthorCSVColumnNumbers.SOURCES.getColumnNumbers());
+
+            if (!sourceNames.isEmpty()) {
+                Set<Source> sources = new HashSet<Source>();
+
+                for (int i = 0; i < sourceNames.size(); i++) {
+                    sources.add(new Source(sourceNames.get(i)));
+                }
+                author.setSources(sources);
+            }
+
+            author.setNote(getValue(line, AuthorCSVColumnNumbers.NOTE.getColumnNumber()));
 
             authors.add(author);
         }
 
         return authors;
+    }
+
+    private static List<String> getValues(String [] line, int [] colNumber) {
+
+        List<String> result = new ArrayList<String>();
+
+        for (int number : colNumber) {
+            if (getValue(line, number) == null) {
+                continue;
+            }
+            result.add(getValue(line, number));
+        }
+
+        return result;
+    }
+
+    private static String getValue(String [] line, int number) {
+        if (number >= line.length) {
+            return null;
+        }
+
+        return (line[number].trim().isEmpty()) ? null : line[number].trim();
     }
 }
